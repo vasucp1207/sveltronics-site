3510514c39ee7c29b9d566911b76d52030b751f4
vasucp1207 <vasucp1207@gmail.com>
2023-06-10 11:04:49 +0000

    glowing effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -78,13 +78,25 @@
     background-color: #ee9617;
     background-image: linear-gradient(315deg, #ee9617 0%, #fe5858 74%);
 
-    box-shadow: 0px -5px 20px 1px #ff61a0, 0px 10px 40px 1px #ff824c;
+    /* box-shadow: 0px -5px 20px 1px #ff61a0, 0px 10px 40px 1px #ff824c; */
+    filter: drop-shadow(0 0 35px #ff824c);
+    animation-name: glow;
+    animation-duration: 4s;
+    animation-direction: normal;
+    animation-iteration-count: infinite;
   }
+  
+    @keyframes glow {
+      0% { filter: drop-shadow(10px 20px 35px #ff824c) }
+      25% { filter: drop-shadow(10px 10px 45px #f3713a) }
+      50% { filter: drop-shadow(10px 20px 55px #fe5858) }
+      75% { filter: drop-shadow(10px 30px 65px #fe5858) }
+      100% { filter: drop-shadow(10px 20px 35px #ff824c) }
+    }
 
   @media only screen and (max-width: 806px) {
     .btn-cont {
       justify-content: center;
     }
   }
-
 </style>
